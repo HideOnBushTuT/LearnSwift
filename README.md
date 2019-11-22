f293d907279911d0c68264c907c02e771281df25
shanbin cai <shanbin.cai@gmail.com>
2019-11-22 16:33:02 +0800

    tuple

--- README.md
+++ README.md
@@ -12,3 +12,6 @@
 
 
 
+### Tuple 
+
+![Tuple](/Images/tuple.png)
--- playground.playground/Contents.swift
+++ playground.playground/Contents.swift
@@ -2,6 +2,7 @@ import UIKit
 
 var str = "Hello, playground"
 
+// ================ String
 //let maxNumber = 10
 //var currentNumber = 9
 //
@@ -14,11 +15,43 @@ var str = "Hello, playground"
 
 //let 1ab = "abc"
 
-let a = 8
-let b: UInt8 = 255
+// =============== Number
+//let a = 8
+//let b: UInt8 = 255
+//
+//print("Uint8 min: \(UInt8.min), max: \(UInt8.max)")
+//
+//typealias AudioSample = UInt8
+//let sample: AudioSample = 32
 
-print("Uint8 min: \(UInt8.min), max: \(UInt8.max)")
 
-typealias AudioSample = UInt8
-let sample: AudioSample = 32
+// ================ Tuple
+// let error = (1, "没有权限")
+//print(error)
+//
+//print(error.0)
+//print(error.1)
 
+//let error = (errorCode: 1, errorMessage: "没有权限")
+//print(error.errorCode)
+//print(error.errorMessage)
+
+//var error = (errorCode: 1, errorMessage: "没有权限")
+//error.errorCode = 2
+//print(error)
+
+//var error:(errorCode: Int, errorMessage: Any) = (1, "没有权限")
+//error.errorMessage = 3
+//print(error)
+
+//let error = (1, "没有权限")
+//let (errorCode, errorMessage) = error
+//print(errorCode)
+//print(errorMessage)
+
+func writeFile(content: String) -> (errorCode: Int, errorMessage: String) {
+    return (1, "没有权限")
+}
+
+let error = writeFile(content: "")
+print(error)
