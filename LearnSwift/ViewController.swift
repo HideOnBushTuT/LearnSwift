cbb5dfe016e4948be42ae81278c5ba14eaf925fb
shanbin cai <shanbin.cai@gmail.com>
2019-11-22 15:59:31 +0800

    project init

---
+++ LearnSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LearnSwift
+//
+//  Created by CBReno on 2019/11/22.
+//  Copyright © 2019 CBReno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let a = 10
+        var b = 9
+        var c = 10, d = 11, f = 12
+        
+        var g: String
+        g = "hello swift"
+        
+    }
+
+
+}
+
