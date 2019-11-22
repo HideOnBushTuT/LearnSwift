add909353b4dbde1a93ba362a04584096720322a
shanbin cai <shanbin.cai@gmail.com>
2019-11-22 16:01:20 +0800

    变量与常量

---
+++ variable.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let maxNumber = 10
+var currentNumber = 9
+
+//maxNumber = 0
+currentNumber = 10
+
+let x = 0.0, y = 0.0, z = "0.0"
+
+print("x = \(x), y = \(y), z = \(z)")
+
+//let 1ab = "abc"
